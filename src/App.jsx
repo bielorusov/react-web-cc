f0d8b11521cc9e0057e3f6b9dcc94f4f8bd725b7
Dmytro Bielorusov <dmytro.bielorusov@gmail.com>
2021-01-09 04:07:17 +0200

    Initial Repo

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import {BrowserRouter, Route, Switch} from "react-router-dom"
+import {Home} from "./pages/Home";
+import {About} from "./pages/About";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="container pt-4">
+        <Switch>
+          <Route path={'/'} exact component={Home}/>
+          <Route path={'/about'} component={About}/>
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
