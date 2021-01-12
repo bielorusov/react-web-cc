3a2f25b8ab08e667a8d7216de69960a5cd1cf09d
Dmytro Bielorusov <dmytro.bielorusov@gmail.com>
2021-01-12 14:02:24 +0200

    Update Spinners

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
 
+// Style guide RFC: https://getbootstrap.com/docs/5.0/components/spinners/
 export const Loader = () => (
   <div className="text-center">
-    <div className="spinner-border" role="status">
+    <div className="spinner-border text-success">
+      <span className="visually-hidden">Loading...</span>
+    </div>
+    <div className="spinner-border text-warning">
+      <span className="visually-hidden">Loading...</span>
+    </div>
+    <div className="spinner-border text-primary">
       <span className="visually-hidden">Loading...</span>
     </div>
   </div>
