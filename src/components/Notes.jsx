e3248b7a8ac3bacf120cad83f74ddf8d3c392603
Dmytro Bielorusov <dmytro.bielorusov@gmail.com>
2021-01-12 13:22:09 +0200

    Alert for Deletion & no items

--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -1,28 +1,43 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import {TransitionGroup, CSSTransition} from 'react-transition-group'
+import {AlertContext} from "../context/alert/alertContext";
 
-export const Notes = ({notes, onRemove}) => (
-  <TransitionGroup component="ul" className="list-group">
-    {notes.map(note => (
-      <CSSTransition
-        key={note.id}
-        classNames={'note'}
-        timeout={800}
-      >
-        <li className="list-group-item note">
-          <div>
-            <strong>{note.title}</strong>
-            <small>{note.date}</small>
-          </div>
-          <button
-            type="button"
-            className="btn btn-outline-danger btn-sm"
-            onClick={() => onRemove(note.id)}
-          >
-            &times;
-          </button>
-        </li>
-      </CSSTransition>
-    ))}
-  </TransitionGroup>
-)
+export const Notes = ({notes, onRemove}) => {
+  const alert = useContext(AlertContext)
+
+  const onRemoveHandler = (note_id) => {
+    onRemove(note_id).then(() => {
+      alert.show('Заметка была удалена', 'info')
+    }).catch(() => {
+      alert.show('Что-то пошло не так', 'danger')
+    })
+  }
+
+  if(notes.length == 0) { return(<p className="text-center">Заметок пока нет!</p>) }
+
+  return (
+    <TransitionGroup component="ul" className="list-group">
+      {notes.map(note => (
+        <CSSTransition
+          key={note.id}
+          classNames={'note'}
+          timeout={800}
+        >
+          <li className="list-group-item note">
+            <div>
+              <strong>{note.title}</strong>
+              <small>{note.date}</small>
+            </div>
+            <button
+              type="button"
+              className="btn btn-outline-danger btn-sm"
+              onClick={() => onRemoveHandler(note.id)}
+            >
+              &times;
+            </button>
+          </li>
+        </CSSTransition>
+      ))}
+    </TransitionGroup>
+  )
+}
